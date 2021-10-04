ac0a5e3f0aeb4f26d85a20d2e71daa1d53ccbb7f
Daniil Kudinov <texhnolyzed.soul@gmail.com>
2021-10-04 15:31:51 +0300

    homework 4

--- homework-4.js
+++ homework-4.js
@@ -22,7 +22,7 @@ console.log(arrToCamelCase(arr2));
 //(наприклад 1 = a, 2 = b, 3 = c, 4 = d, …).Приклад[4, 3, 22, 11] - в результаті маємо отримати[d, c, v, k]
 
 
-const arr = [1, 2, 27, -5, 5.5, 3];
+const arr = [1, 2, 27, -5, 5.5, 3, 26];
 
 function numberToLetter(arr) {
   return arr
@@ -47,6 +47,8 @@ const object = {
   d: 'hello',
   e: 228,
   f: '228',
+	g: true,
+	h: []
 };
 
 Object.filter = (obj, predicate) =>
@@ -62,24 +64,50 @@ console.log(filtered);
 
 //Скрипт який виводить в консоль дату і час коли DOM побудований, але css, зображення ще не завантажились.
 
-
+document.addEventListener('DOMContentLoaded', () => {
+	console.log(`DOM tree is build ${new Date()}`);
+});
 
 //Скрипт який виводить в консоль дату і час коли DOM побудований і css, зображення вже завантажились.
 
-
+window.onload = () => {
+	console.log(`Page is loaded ${new Date()}`);
+};
 
 //Скрипт який виводить в консоль дату і час коли юзер натиснув на закрити вкладку або перезавантажити сторінку.
 
-
+window.onbeforeunload = function () {
+return console.log(`${new Date()}`);
+};
 
 //Скрипт який виводить повертає назву браузера та назву операційної системи через дефіс "-" як одну строку.
 
+let OSName = 'Unknown OS';
 
+if (navigator.appVersion.indexOf('Win') != -1) OSName = 'Windows';
+if (navigator.appVersion.indexOf('Mac') != -1) OSName = 'MacOS';
+if (navigator.appVersion.indexOf('X11') != -1) OSName = 'UNIX';
+if (navigator.appVersion.indexOf('Linux') != -1) OSName = 'Linux';
+
+let sBrowser = 'unknown',
+  sUsrAg = navigator.userAgent;
+
+if (sUsrAg.indexOf('Firefox') > -1) { sBrowser = 'Mozilla Firefox'; }
+else if (sUsrAg.indexOf('Opera') > -1) { sBrowser = 'Opera'; }
+else if (sUsrAg.indexOf('Trident') > -1) {sBrowser = 'Microsoft Internet Explorer';}
+else if (sUsrAg.indexOf('Edge') > -1) {sBrowser = 'Microsoft Edge';}
+else if (sUsrAg.indexOf('Chrome') > -1) {sBrowser = 'Google Chrome';}
+else if (sUsrAg.indexOf('Safari') > -1) { sBrowser = 'Apple Safari';}
+
+console.log(`${sBrowser} - ${OSName} `);
 
 //Скрипт який змушує браузер показувати сповіщення, коли ви намагаєтесь перезавантажити сторінку, 
 //видалити вкладку, чи вимкнути браузер.
 
-
+window.onbeforeunload = function () {
+  return false;
+};
 
 //Скрипт який виводить в консоль скільки секунд юзер перебував на сторінці, після того як юзер 
 //натиснув на закрити вкладку або перезавантажити сторінку.
+
